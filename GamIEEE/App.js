897b6eee0211c546d5e32efdac401328b496bb1b
Daniel André <daniel.oliveira4142@gmail.com>
2020-07-12 15:56:19 -0300

    Criação do template básico
    Expo init e personalização do texto do template básico

---
+++ GamIEEE/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá! Bem Vindo ao GamIEEE</Text>
+      <Text>Esse é o template básico do app de gamification do nosso ramo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
